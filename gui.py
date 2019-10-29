ba73af1bd4db86a0a1a099b25811d0f0f9540294
Florian Delage <delageflo@ymail.com>
2019-10-29 10:15:19 +0100

    initialize Gui images from constants.py

--- gui.py
+++ gui.py
@@ -16,6 +16,14 @@ class Gui:
     def __init__(self):
         pygame.init()
         pygame.font.init()
+        self.labyrinth = Labyrinth(MAP_FILE)
+        self.window = pygame.display.set_mode((WINDOW_SIDE, WINDOW_SIDE))
+        self.window_title = pygame.display.set_caption(WINDOW_TITLE)
+        self.window_icon = pygame.image.load(ICON_IMG).convert_alpha()
+        self.wall_img = pygame.image.load(WALL_IMG).convert_alpha()
+        self.floor_img = pygame.image.load(FLOOR_IMG).convert_alpha()
+        self.macgyver_img = pygame.image.load(PLAYER_CHARACTER_IMG).convert_alpha()
+        self.guardian_img = pygame.image.load(NON_PLAYER_CHARACTER_IMG).convert_alpha()
 
 
             
